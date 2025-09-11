3315f0b46259ea9fefc104bba6ea8d0774510060
Esther131213 <estherlimalmgren@gmail.com>
2025-09-11 11:25:18 +0200

    Figuring out how to work Player Class

--- SpaceGame/Enemy.cs
+++ SpaceGame/Enemy.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Drawing;
+
 
 namespace SpaceGame
 {
     internal class Enemy
     {
+        Texture2D texture;
+        Vector2 position;
+
+        public Enemy(Texture2D texture, Vector2 position)
+        {
+
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
     }
 }
--- SpaceGame/Player.cs
+++ SpaceGame/Player.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Text;
 using System.Linq;
-using System.Numerics;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Drawing;
+
 
 namespace SpaceGame
 {
-    internal class Player()
+    internal class Player
     {
         Texture2D texture;
-        Vector2 postition;
+        Vector2 position;
+        int playerSpeed = 10;
+
+        public Player(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+
+            this.position = new Vector2(100, 100);
+        }
+
+        public void Load()
+        {
+            var kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Left))
+            {
+                position.X += playerSpeed;
+            }
+            if (kstate.IsKeyDown(Keys.Left))
+            {
+                position.X -= playerSpeed;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Microsoft.Xna.Framework.Color.White);
+        }
     }
 }
